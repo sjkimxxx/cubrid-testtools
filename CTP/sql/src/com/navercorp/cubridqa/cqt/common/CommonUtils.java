712aa49ca2220bd5ec90ca097312ca39b90dd11d
sjkimxxx <42533491+sjkimxxx@users.noreply.github.com>
2024-11-04 12:50:05 +0900

    Update CommonUtils.java

--- CTP/sql/src/com/navercorp/cubridqa/cqt/common/CommonUtils.java
+++ CTP/sql/src/com/navercorp/cubridqa/cqt/common/CommonUtils.java
@@ -24,7 +24,7 @@
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.navercorp.cubridqa.cqt.common;
-  
+      
 import com.navercorp.cubridqa.cqt.console.util.TestUtil;
 import java.io.BufferedInputStream;
 import java.io.File;
