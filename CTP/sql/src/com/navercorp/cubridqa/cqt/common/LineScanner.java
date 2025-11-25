99e7578ea272c78c6ddc39ac4b281b6bff769dd4
sjkimxxx <42533491+sjkimxxx@users.noreply.github.com>
2025-11-25 14:21:53 +0900

    Remove unnecessary whitespace in LineScanner.java

--- CTP/sql/src/com/navercorp/cubridqa/cqt/common/LineScanner.java
+++ CTP/sql/src/com/navercorp/cubridqa/cqt/common/LineScanner.java
@@ -24,7 +24,7 @@
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.navercorp.cubridqa.cqt.common;
-
+  
 // This class is a porting of csql_walk_statement() function
 // contained in $CUBRID/src/executables/csql_support.c
 
